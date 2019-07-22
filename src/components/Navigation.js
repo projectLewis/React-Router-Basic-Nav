4587b64522b570fa676cf87b6b47e07417aab7aa
Seth Lewis <lewiscatenary@gmail.com>
2019-07-22 13:40:49 -0500

    MVP met

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "../components/Home";
+import About from "../components/About";
+import Contact from "../components/Contact";
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <Router>
+          <h1>React Router Mini</h1>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
